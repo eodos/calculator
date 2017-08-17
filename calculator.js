09cb2dee079e23c8a2fa8632d70a51b4eb1aafd7
David Paul Pena <dppena@magicleap.com>
2017-08-17 10:26:04 -0700

    Fix some operations

--- calculator.js
+++ calculator.js
@@ -1,8 +1,28 @@
+// Button handlers
 $(document).ready(() => {
 	$('button').click(function() {
 		let entry = $(this).attr('value');
 		parseKey(entry);
 	});
+});
+
+// Key press handlers
+$(document).keypress(function(event) {
+	console.log('Key pressed: ' + event.key);
+	switch (event.key) {
+		case 'Enter':
+			parseKey('=');
+			break;
+		case '/':
+			parseKey('%');
+			break;
+		case '*':
+			parseKey('x');
+			break;
+		default:
+			parseKey(event.key);
+			break;
+	}
 });
 
 var digits = 0;
@@ -12,40 +32,52 @@ var calculatorHistory = '0';
 var operation = '';
 var operationFinished = false;
 var partialSolution = 0;
+var showResult = false;
 
 function parseKey(key) {
 	console.log('Entry: ' + key);
 
 	if ($.isNumeric(key)) {
 		parseNumber(key);
+	} else {
+		switch (key) {
+			case '.':
+				parseDecimal();
+				break;
+			case '=':
+				solve();
+				break;
+			case '+':
+			case '-':
+				parseOperator(key);
+				break;
+			case 'ce':
+				entry = '0';
+				digits = 0;
+				break;
+			case 'ac':
+				reset();
+				break;
+		}
 	}
-	switch (key) {
-		case '.':
-			parseDecimal();
-			break;
-		case '=':
-			solve();
-			break;
-		case '+':
-			parseOperator(key);
-			break;
-		case 'ac':
-			reset();
-			break;
-	}
+
+	// Update display
+	$('#current').text(entry);
+	$('#history').text(calculatorHistory);
 
 	console.log('Digits: ' + digits);
 }
 
 function parseNumber(key) {
-	entry = entry === '0' ? key : entry.concat(key);
-	digits = entry === '0' ? 0 : digits + 1;
+	entry = entry === '0' || showResult ? key : entry.concat(key);
+	digits = (entry === '0' || showResult) && key === '0' ? 0 : digits + 1;
+	showResult = false;
 
 	if (digits == 10) {
 		reset();
-		return;
+		entry = 'Max digits';
+		showResult = true;
 	}
-	$('#current').text(entry);
 }
 
 function parseDecimal() {
@@ -53,55 +85,54 @@ function parseDecimal() {
 		isDecimal = true;
 		digits++;
 		entry = entry.concat('.');
-		$('#current').text(entry);
 	}
 }
 
 function parseOperator(operator) {
+	if (operationFinished) {
+		operationFinished = false;
+		calculatorHistory = '0';
+	}
+	operation = operator;
+	parsePartialSolution(entry);
+
 	calculatorHistory =
 		calculatorHistory === '0' ? entry + operator : calculatorHistory.concat(entry + operator);
-
-	partialSolution = Number(entry);
-	operation = operator;
 	console.log('Operator: ' + operation);
 	console.log('History: ' + calculatorHistory);
 
-	// switch (operator) {
-	// 	case '+':
-	// 		break;
-	// }
-
-	entry = '0';
+	entry = String(partialSolution);
+	showResult = true;
 	isDecimal = false;
 	digits = 0;
-	$('#history').text(calculatorHistory);
-	$('#current').text(entry);
+}
+
+function parsePartialSolution(input) {
+	switch (operation) {
+		case '+':
+			partialSolution += Number(input);
+			break;
+		case '-':
+			partialSolution -= Number(input);
+			break;
+	}
+	console.log('Partial solution: ' + input);
 }
 
 function solve() {
 	if (calculatorHistory == '0') {
 		calculatorHistory = entry;
-	} else {
-		calculatorHistory = calculatorHistory.concat(entry + '=');
-		console.log(entry + ' ' + calculatorHistory);
-		switch (operation) {
-			case '+':
-				console.log('Adding');
-				partialSolution += Number(entry);
-				break;
-		}
-		entry = partialSolution;
-		console.log('Solution: ' + entry);
+	} else if (calculatorHistory.charAt(calculatorHistory.length - 1) !== '=') {
+		parsePartialSolution(entry);
+		calculatorHistory = calculatorHistory.concat(entry + '=' + partialSolution);
 	}
-	$('#history').text(calculatorHistory);
-	$('#current').text(entry);
-	reset();
-}
-
-function maxDigits() {
-	console.log('Max digits reached');
-	$('#current').text('Max digits reached');
-	reset();
+	// reset();
+	operationFinished = true;
+	showResult = true;
+	history = '0';
+	operation = '';
+	partialSolution = 0;
+	// TODO: fix reset() to do a partial re-initialization of variables
 }
 
 function reset() {
